51511e0a2edcce37235b467cf8dbfe956cfc5dc0
natsumo <ikedana@systena.co.jp>
2016-05-31 19:47:58 +0900

    1
    20160531

---
+++ ObjcFileApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ObjcFileApp
+//
+//  Created by Natsumo Ikeda on 2016/05/31.
+//  Copyright © 2016年 NIFTY Corporation. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ObjcFileApp/ViewController.h
@@ -0,0 +1,16 @@
+//
+//  ViewController.h
+//  ObjcFileApp
+//
+//  Created by Natsumo Ikeda on 2016/05/31.
+//  Copyright © 2016年 NIFTY Corporation. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "NCMB/NCMB.h"
+
+@interface ViewController : UIViewController<UIImagePickerControllerDelegate, UINavigationControllerDelegate>
+
+
+@end
+
