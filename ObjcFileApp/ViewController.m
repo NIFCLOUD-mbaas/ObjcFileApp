507d3e701811642975bfbd4c642691376374f041
Cu Cong Can <cancc@vitalify.asia>
2020-09-25 10:33:07 +0700

    update copyright and readme

--- ObjcFileApp/ViewController.m
+++ ObjcFileApp/ViewController.m
@@ -3,7 +3,7 @@
 //  ObjcFileApp
 //
 //  Created by Natsumo Ikeda on 2016/05/31.
-//  Copyright 2017 FUJITSU CLOUD TECHNOLOGIES LIMITED All Rights Reserved.
+//  Copyright 2020 FUJITSU CLOUD TECHNOLOGIES LIMITED All Rights Reserved.
 //
 
 #import "ViewController.h"
